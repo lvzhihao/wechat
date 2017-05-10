a181146783e280a500249f6f5bd38374022289e9
edwin <edwin.lzh@gmail.com>
2017-05-10 16:04:17 +0800

    fix

--- cmd/server.go
+++ cmd/server.go
@@ -39,13 +39,10 @@ import (
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Wechat Proxy Server",
+	Long: `Auto Refresh Access Token. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+wechat server --app_id=xxxx --app_secret=xxxx`,
 	Run: func(cmd *cobra.Command, args []string) {
 		//logger
 		logger, _ := zap.NewProduction()
