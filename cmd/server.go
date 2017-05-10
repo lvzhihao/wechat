946797e168403c72834ed80175cd220ba8a13612
edwin <edwin.lzh@gmail.com>
2017-05-10 16:38:57 +0800

    fix

--- cmd/server.go
+++ cmd/server.go
@@ -24,6 +24,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"io"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -59,15 +60,20 @@ wechat server --app_id=xxxx --app_secret=xxxx`,
 		}
 		logger.Info("Wechat Connecting...", zap.String("token", client.FetchToken()))
 		hijack(client, logger)
-		logger.Info("Proxy Running...", zap.String("addr", ":9099"))
-		logger.Fatal("Proxy Stop...", zap.Any("info", http.ListenAndServeTLS(":9099", "./pem/server.cert", "./pem/server.key", nil)))
+		logger.Info("Proxy Running...", zap.String("addr", viper.GetString("proxy_addr")))
+		logger.Fatal("Proxy Stop...", zap.Any("info", http.ListenAndServeTLS(
+			viper.GetString("proxy_addr"),
+			viper.GetString("tls_cert"),
+			viper.GetString("tls_key"),
+			nil,
+		)))
 	},
 }
 
 func errResult(code int, msg string) string {
 	b, _ := json.Marshal(struct {
-		errcode int
-		errmsg  string
+		errcode int    `json:"errcode"`
+		errmsg  string `json:"errmsg"`
 	}{
 		code,
 		msg,
@@ -75,9 +81,21 @@ func errResult(code int, msg string) string {
 	return string(b)
 }
 
+func randStringRunes(n int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
 func hijack(c *core.Client, l *zap.Logger) {
 	sugar := l.Sugar()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestId := randStringRunes(40)
+		w.Header().Set("X-REQUEST-ID", requestId)
+
 		hj, ok := w.(http.Hijacker)
 		if !ok {
 			http.Error(w, errResult(-2, "not a hijacker"), http.StatusInternalServerError)
@@ -86,7 +104,7 @@ func hijack(c *core.Client, l *zap.Logger) {
 
 		in, _, err := hj.Hijack()
 		if err != nil {
-			l.Error("Hijack error", zap.Any("url", r.URL), zap.Error(err))
+			l.Error("Hijack error", zap.Any("url", r.URL), zap.Error(err), zap.String("request_id", requestId))
 			http.Error(w, errResult(-2, "hijack error"), http.StatusInternalServerError)
 			return
 		}
@@ -106,7 +124,7 @@ func hijack(c *core.Client, l *zap.Logger) {
 		out, _ := httputil.NewClientConn(tls_conn, nil).Hijack()
 		err = r.Write(out)
 		if err != nil {
-			l.Error("Error copying request", zap.Any("url", r.URL), zap.Error(err))
+			l.Error("Error copying request", zap.Any("url", r.URL), zap.Error(err), zap.String("request_id", requestId))
 			http.Error(w, errResult(-2, "error copying request"), http.StatusInternalServerError)
 			return
 		}
@@ -124,13 +142,15 @@ func hijack(c *core.Client, l *zap.Logger) {
 		go cp(in, out)
 		err = <-errc
 		if err != nil && err != io.EOF {
-			l.Error("proxy error", zap.Any("url", r.URL), zap.Error(err))
+			l.Error("proxy error", zap.Any("url", r.URL), zap.Error(err), zap.String("request_id", requestId))
 		}
 	})
 }
 
 func init() {
 	RootCmd.AddCommand(serverCmd)
+
+	rand.Seed(time.Now().UnixNano())
 
 	// Here you will define your flags and configuration settings.
 
@@ -143,4 +163,7 @@ func init() {
 	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	viper.Set("app_id", serverCmd.Flags().String("app_id", "", "第三方用户唯一凭证"))
 	viper.Set("app_secret", serverCmd.Flags().String("app_secret", "", "第三方用户唯一凭证密钥"))
+	viper.Set("proxy_addr", serverCmd.Flags().String("proxy_addr", ":9099", "代理监听地址"))
+	viper.Set("tls_cert", serverCmd.Flags().String("tls_cert", "./pem/server.cert", "ssl证书"))
+	viper.Set("tls_key", serverCmd.Flags().String("tls_key", "./pem/server.key", "ssl证书"))
 }
